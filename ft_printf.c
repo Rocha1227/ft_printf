35387a809ffe2ebf38c502c16f98813e81b72e12
Kévin <krocha@Krocha>
2023-06-02 00:23:24 +0100

    starting point

---
+++ ft_printf.c
@@ -0,0 +1,26 @@
+int	printf(const char *amat, ...);
+
+//print a caracter - %c
+
+int	ft_printfchar(char c )
+{
+	write(1, &c, 1);	
+}
+
+//print a string - %s
+
+
+
+//print a void * pointer return hexadecimal format - %p
+
+//print a decimal (base 10) number - %d
+
+//print an integer in base 10 - %i
+
+//print an unsigned decimal ( base 10) number - %u
+
+//print a hexadecimal (base 16) lowercase format - %x
+
+//print a hexadecimal (base 16) uppercase format - %X
+
+//print a percent sign - %%
